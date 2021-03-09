cc4a559fd2e1236904095abe859a69fa0dc0550c
stephen.brooks220413 <stephen.brooks220413@gmail.com>
2021-03-09 14:55:57 -0800

    updates

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,124 @@
 <template>
   <div class="hello container">
+    <!--content below top image-->
+    <div class="container text-info spacer">
+      <div class="row">
+        <div class="col-md">
+          <div class="bg"></div>
+        </div>
+        <div class="col-md">
+          <h1>#1 Rated for Online Learning</h1>
+          <h2>High Deliverable Content</h2>
+          <h3>Experienced Instructors</h3>
+          <h4>Learn Valuable Skills</h4>
+        </div>
+      </div>
+    </div>
+    <!--languages block-->
+    <div class="angle">
+      <h1 class="display-4">Languages Used</h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-md">
+            <br />
+            <img
+              src="https://clipartart.com/images/html-logo-clipart-6.jpg"
+              alt="html"
+              class="max img-fluid"
+            />
+          </div>
+          <div class="col-md">
+            <br />
+            <img
+              src="https://1000logos.net/wp-content/uploads/2020/09/CSS-Logo.jpg"
+              alt="css"
+              class="max img-fluid"
+            />
+          </div>
+          <div class="col-md">
+            <br />
+            <img
+              src="https://www.miltonmarketing.com/wp-content/uploads/2018/04/Javascript_logo-1170x850.jpg"
+              alt="javascript"
+              class="max img-fluid"
+            />
+          </div>
+          <div class="col-md">
+            <br />
+            <img
+              src="https://res.cloudinary.com/eternitech/image/upload/$wpsize_!_cld_full!,w_400,h_400,c_scale/v1588246109/eternitech/nodejs.png"
+              alt="framework"
+              class="max img-fluid"
+            />
+          </div>
+        </div>
+        <br /><br />
+        <!--second row-->
+        <div class="row">
+          <div class="col-md">
+            <img
+              src="https://miro.medium.com/max/3954/1*w0u2TZpEp3WfKMrlL5jTSw.png"
+              alt="C#"
+              class="max img-fluid"
+            />
+          </div>
+          <div class="col-md">
+            <img
+              src="https://www.amittyagi.com/static/img/sql.jpg"
+              alt="SQL"
+              class="max img-fluid"
+            />
+          </div>
+          <div class="col-md">
+            <img
+              src="https://th.bing.com/th/id/Rdba9a27a945a1fca3d1d6a9b2fa45f03?rik=wKjPIUA2HZCEkA&riu=http%3a%2f%2fwww.tridiondeveloper.com%2fwp-content%2fuploads%2f2013%2f09%2flogo-json.png&ehk=UF76lJ8jVOrNw9ZRqfS4flM%2bfeo7ylUfeut75nmI25E%3d&risl=&pid=ImgRaw"
+              alt="json"
+              class="max img-fluid"
+            />
+          </div>
+          <div class="col-md">
+            <img
+              src="https://cms-assets.tutsplus.com/uploads/users/71/courses/419/preview_image/typescript.png"
+              alt="framework"
+              class="max img-fluid"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
     <h1 class="display-4">{{ msg }}</h1>
     <br />
     <form name="contact" netlify>
       <!-- Name input -->
       <div class="form-outline mb-4">
-        <input type="text" id="form4Example1" name="name" class="form-control" />
+        <input
+          type="text"
+          id="form4Example1"
+          name="name"
+          class="form-control"
+        />
         <label class="form-label" for="form4Example1">Name</label>
       </div>
 
       <!-- Email input -->
       <div class="form-outline mb-4">
-        <input type="email" id="form4Example2" name="email" class="form-control" />
+        <input
+          type="email"
+          id="form4Example2"
+          name="email"
+          class="form-control"
+        />
         <label class="form-label" for="form4Example2">Email address</label>
       </div>
 
       <!-- Message input -->
       <div class="form-outline mb-4">
-        <textarea class="form-control" name="message" id="form4Example3" rows="4"></textarea>
+        <textarea
+          class="form-control"
+          name="message"
+          id="form4Example3"
+          rows="4"
+        ></textarea>
         <label class="form-label" for="form4Example3">Message</label>
       </div>
 
@@ -54,6 +155,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "../assets/styles.scss";
 .hello {
   padding-bottom: 120px;
   max-width: 650px;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,13 +3,15 @@
     <!-- code here -->
     <div class="logo">
       <p class="contact-info">
-      <a href="#" class="btn round btn-primary">LEARN MORE</a>
+        <a href="#" class="btn round btn-primary">LEARN MORE</a>
       </p>
     </div>
     <br />
     <!--title-->
     <div class="author">
-      <img src="https://www.clear-web-designs.com/assets/img/author.jpg" alt="author"
+      <img
+        src="https://www.clear-web-designs.com/assets/img/author.jpg"
+        alt="author"
         class="img-fluid img1"
       />
       <div class="container p-4 pb-0">
@@ -30,7 +32,7 @@
             style="background-color: #55acee;"
             href="#!"
             role="button"
-            ><i class="fab fa-twitter"></i
+            ><i class="fas fa-headset"></i
           ></a>
 
           <!-- Google -->
@@ -39,16 +41,7 @@
             style="background-color: #dd4b39;"
             href="#!"
             role="button"
-            ><i class="fab fa-google"></i
-          ></a>
-
-          <!-- Instagram -->
-          <a
-            class="btn btn-primary btn-floating m-1"
-            style="background-color: #ac2bac;"
-            href="#!"
-            role="button"
-            ><i class="fab fa-instagram"></i
+            ><i class="fas fa-at"></i
           ></a>
 
           <!-- Linkedin -->
@@ -75,11 +68,10 @@
       <h4>Founder / Developer: Stephen Brooks</h4>
       <hr />
       <p>
-        Graduate of Bryan University with and Assoicate Degree in
-        Fullstack Development, Stephen offers courses in languages
-        that Employers are currently hiring for. Stephen is located
-        Oklahoma, previously served in the Army for 17 years and
-        retired from Engineering.
+        Graduate of Bryan University with and Assoicate Degree in Fullstack
+        Development, Stephen offers courses in languages that Employers are
+        currently hiring for. Stephen is located Oklahoma, previously served in
+        the Army for 17 years and retired from Engineering.
       </p>
     </div>
     <br /><br />
@@ -87,7 +79,9 @@
       <div class="row">
         <div class="col-md">
           <div class="author">
-            <img src="https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="author"
+            <img
+              src="https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+              alt="author"
               class="img-fluid img"
               style="height: 200px; width: 200px;"
             />
@@ -109,7 +103,7 @@
                   style="background-color: #55acee;"
                   href="#!"
                   role="button"
-                  ><i class="fab fa-twitter"></i
+                  ><i class="fas fa-headset"></i
                 ></a>
 
                 <!-- Google -->
@@ -118,17 +112,8 @@
                   style="background-color: #dd4b39;"
                   href="#!"
                   role="button"
-                  ><i class="fab fa-google"></i
-                ></a>
-
-                <!-- Instagram -->
-                <a
-                  class="btn btn-primary btn-floating m-1"
-                  style="background-color: #ac2bac;"
-                  href="#!"
-                  role="button"
-                  ><i class="fab fa-instagram"></i
-                ></a>
+                  ><i class="fas fa-at"></i>></a
+                >
 
                 <!-- Linkedin -->
                 <a
@@ -160,7 +145,9 @@
         </div>
         <div class="col-md">
           <div class="author">
-            <img src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="author"
+            <img
+              src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+              alt="author"
               class="img-fluid img"
               style="height: 200px; width: 200px;"
             />
@@ -182,7 +169,7 @@
                   style="background-color: #55acee;"
                   href="#!"
                   role="button"
-                  ><i class="fab fa-twitter"></i
+                  ><i class="fas fa-headset"></i
                 ></a>
 
                 <!-- Google -->
@@ -191,16 +178,7 @@
                   style="background-color: #dd4b39;"
                   href="#!"
                   role="button"
-                  ><i class="fab fa-google"></i
-                ></a>
-
-                <!-- Instagram -->
-                <a
-                  class="btn btn-primary btn-floating m-1"
-                  style="background-color: #ac2bac;"
-                  href="#!"
-                  role="button"
-                  ><i class="fab fa-instagram"></i
+                  ><i class="fas fa-at"></i
                 ></a>
 
                 <!-- Linkedin -->
@@ -233,7 +211,9 @@
         </div>
         <div class="col-md">
           <div class="author">
-            <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="author"
+            <img
+              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+              alt="author"
               class="img-fluid img"
               style="height: 200px; width: 200px;"
             />
@@ -255,7 +235,7 @@
                   style="background-color: #55acee;"
                   href="#!"
                   role="button"
-                  ><i class="fab fa-twitter"></i
+                  ><i class="fas fa-headset"></i
                 ></a>
 
                 <!-- Google -->
@@ -264,16 +244,7 @@
                   style="background-color: #dd4b39;"
                   href="#!"
                   role="button"
-                  ><i class="fab fa-google"></i
-                ></a>
-
-                <!-- Instagram -->
-                <a
-                  class="btn btn-primary btn-floating m-1"
-                  style="background-color: #ac2bac;"
-                  href="#!"
-                  role="button"
-                  ><i class="fab fa-instagram"></i
+                  ><i class="fas fa-at"></i
                 ></a>
 
                 <!-- Linkedin -->
@@ -306,86 +277,6 @@
         </div>
       </div>
     </div>
-    <!--content below top image-->
-    <div class="container text-info spacer">
-      <div class="row">
-        <div class="col-md">
-          <div class="bg"></div>
-        </div>
-        <div class="col-md">
-          <h1>#1 Rated for Online Learning</h1>
-          <h2>High Deliverable Content</h2>
-          <h3>Experienced Instructors</h3>
-          <h4>Learn Valuable Skills</h4>
-        </div>
-      </div>
-    </div>
-    <!--languages block-->
-    <div class="angle">
-      <h1 class="display-4">Languages Used</h1>
-     <div class="container">
-      <div class="row">
-        <div class="col-md">
-        <br />
-          <img src="https://clipartart.com/images/html-logo-clipart-6.jpg"
-            alt="html"
-            class="max img-fluid"
-          />
-        </div>
-        <div class="col-md">
-        <br />
-          <img src="https://1000logos.net/wp-content/uploads/2020/09/CSS-Logo.jpg"
-            alt="css"
-            class="max img-fluid"
-          />
-        </div>
-        <div class="col-md">
-        <br />
-          <img
-            src="https://www.miltonmarketing.com/wp-content/uploads/2018/04/Javascript_logo-1170x850.jpg"
-            alt="javascript"
-            class="max img-fluid"
-          />
-        </div>
-        <div class="col-md">
-        <br />
-          <img src="https://res.cloudinary.com/eternitech/image/upload/$wpsize_!_cld_full!,w_400,h_400,c_scale/v1588246109/eternitech/nodejs.png"
-            alt="framework"
-            class="max img-fluid"
-          />
-        </div>
-      </div>
-      <br /><br />
-      <!--second row-->
-      <div class="row">
-        <div class="col-md">
-          <img src="https://miro.medium.com/max/3954/1*w0u2TZpEp3WfKMrlL5jTSw.png"
-            alt="C#"
-            class="max img-fluid"
-          />
-        </div>
-        <div class="col-md">
-          <img src="https://www.amittyagi.com/static/img/sql.jpg"
-            alt="SQL"
-            class="max img-fluid"
-          />
-        </div>
-        <div class="col-md">
-          <img
-            src="https://th.bing.com/th/id/Rdba9a27a945a1fca3d1d6a9b2fa45f03?rik=wKjPIUA2HZCEkA&riu=http%3a%2f%2fwww.tridiondeveloper.com%2fwp-content%2fuploads%2f2013%2f09%2flogo-json.png&ehk=UF76lJ8jVOrNw9ZRqfS4flM%2bfeo7ylUfeut75nmI25E%3d&risl=&pid=ImgRaw"
-            alt="json"
-            class="max img-fluid"
-          />
-        </div>
-        <div class="col-md">
-          <img src="https://cms-assets.tutsplus.com/uploads/users/71/courses/419/preview_image/typescript.png"
-            alt="framework"
-            class="max img-fluid"
-          />
-        </div>
-      </div>
-     </div>
-    </div>
     <HelloWorld msg="Contact Us" />
   </div>
 </template>
@@ -403,90 +294,5 @@ export default {
 </script>
 
 <style lang="scss">
-.logo { 
-  background: url(../assets/logo.png) no-repeat center center; 
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  width: 100%;
-  max-height: 90vh;
-  margin-bottom: 110px;
-
-  .contact-info {
-    padding-top: 40%;
-  }
-
-  .round {
-    border-radius: 34px;
-    font-size: 1em;
-  }
-}
-.author {
-  max-width: 550px;
-  max-height: 400px;
-  margin-left: auto;
-  margin-right: auto;
-  justify-content: center;
-  text-align: center;
-  margin-top: 90px;
-  margin-bottom: 90px;
-
-  img {
-    border-radius: 50%;
-    max-width: 250px;
-    max-height: 250px;
-  }
-
-  hr {
-    width: 270px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-  p {
-    width: 80%;
-    text-align: center;
-    justify-content: center;
-    margin-left: auto;
-    margin-right: auto;
-  }
-}
-
-.angle {
-  background-color: white; 
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  width: 100%;
-  height: auto;
-  padding-top: 40px;
-  padding-bottom: 40px;
-  margin-bottom: 110px;
-  h1 {
-    margin-bottom: 50px;
-  }
-}
-.img {
-  border-radius: 50%;
-  max-width: 300px !important;
-  max-height: 250px;
-}
-.max {
-  height: 150px;
-}
-.spacer {
-  margin-top: 130px;
-  margin-bottom: 90px;
-}
-.bg {
-  background: url(https://bostonstartupsguide.com/wp-content/uploads/2013/12/LaunchAcademyProgrammingBootcamp.jpg)
-    no-repeat center center fixed; 
-  -webkit-background-size: cover;
-  -moz-background-size: cover;
-  -o-background-size: cover;
-  background-size: cover;
-  width: 100%;
-  height: 300px;
-}
+@import "../assets/styles.scss";
 </style>
